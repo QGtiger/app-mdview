da08303e9d48a78524747c0d83e972175ecb2dd4
光鱼 <guang.yu@yingdao.com>
2026-06-30 10:32:28 +0800

    style:统一引号格式并移除数据库配置

--- lightfish-server.config.js
+++ lightfish-server.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from '@lightfish/server';
+import { defineConfig } from "@lightfish/server";
 
 export default defineConfig({
   port: 3000,
-  apiDir: './server/router',
-  appName: 'app-mdview',
-  env: 'dev',
-  databaseUrl: 'postgresql://mdview:mdview@localhost:5432/mdview',
+  apiDir: "./server/router",
+  appName: "app-mdview",
+  env: "dev",
 });
